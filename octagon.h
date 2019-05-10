637459fbecd2d21dd77e68e72df781a7f4fa573c
Pierre Abbat <phma@bezitopo.org>
2019-05-10 23:16:16 +0000

    Add octagon file
    After reading the cloud, the next step is to draw an octagon around
    the points and triangulate it.

---
+++ octagon.h
@@ -0,0 +1,21 @@
+/******************************************************/
+/*                                                    */
+/* octagon.h - bound the points with an octagon       */
+/*                                                    */
+/******************************************************/
+/* Copyright 2019 Pierre Abbat.
+ * This file is part of Decisite.
+ * 
+ * Decisite is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Decisite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Decisite. If not, see <http://www.gnu.org/licenses/>.
+ */
