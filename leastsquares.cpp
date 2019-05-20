99eeb226b05a78e9829d4720cc0b19d63f7b2150
Pierre Abbat <phma@bezitopo.org>
2019-05-20 08:11:01 +0000

    Change comment in least squares
    There's no PuppetBar in Decisite. PuppetBar is a class in Bezitopo
    for representing a total station setup and all points shot from it.

--- leastsquares.cpp
+++ leastsquares.cpp
@@ -22,28 +22,12 @@
 
 #include "leastsquares.h"
 
-/* In a PuppetBar representing a total station setup, pole[0] is (0.0.0)
- * and the height of instrument, pole[1] is the backsight and is normally
- * (but doesn't have to be) in the yz-plane with y positive, and the rest are
- * sideshots and foresights.
- * 
- * The backsight, for proper adjustment, should be measured, but many total
- * stations do not measure it. If it is not measured, the azimuth of
- * Pole[1].displacement is kept, but the vertical angle and distance are
- * replaced with those of the target, which could be Pole[0] of another PuppetBar.
- * 
- * A PuppetBar representing a total station setup generates its own two error
- * terms, representing its tilt.
- * 
- * If you foresight a point, then occupy it, then backsight (and measure) it,
- * this generates three sets of three error terms: between the point as
- * foresighted and its actual position, between the instrument occupying it
- * and its actual position, and between the point as backsighted and its actual
- * position. The actual position and all three setups are adjusted.
- */
 using namespace std;
 
 vector<double> linearLeastSquares(matrix m,vector<double> v)
+/* m should be a matrix(a,b) where a>b. If a<b, mtm is singular.
+ * There are b things which can be adjusted to best fit a data.
+ */
 {
   matrix mtm,mt,vmat=columnvector(v),mtv;
   mt=m.transpose();
