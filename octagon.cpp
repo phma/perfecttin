19de0361e32c5a86486852e9d66b3f3e52b6eaa3
Pierre Abbat <phma@bezitopo.org>
2019-06-16 17:06:43 +0000

    Clear the point cloud after copying it to the triangles
    Keeping it was for processing triangles with more dots more often.

--- octagon.cpp
+++ octagon.cpp
@@ -123,8 +123,8 @@ double makeOctagon()
     tri=tri->findt(cloud[n]);
     tri->dots.push_back(cloud[n]);
   }
-  //cloud.clear();
-  //cloud.shrink_to_fit();
+  cloud.clear();
+  cloud.shrink_to_fit();
   for (i=0;i<6;i++)
   {
     cout<<"triangle "<<i<<" has "<<net.triangles[i].dots.size()<<" dots\n";
