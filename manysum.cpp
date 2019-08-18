a134d23b395488feb00cd1f564ebbf001322c592
Pierre Abbat <phma@bezitopo.org>
2019-08-18 03:23:14 +0000

    Return a value from manysum::operator+= and -=

--- manysum.cpp
+++ manysum.cpp
@@ -88,6 +88,7 @@ manysum& manysum::operator+=(double x)
     else
       x=0;
   }
+  return *this;
 }
 
 manysum& manysum::operator-=(double x)
@@ -113,6 +114,7 @@ manysum& manysum::operator-=(double x)
     else
       x=0;
   }
+  return *this;
 }
 
 double pairwisesum(double *a,unsigned n)
