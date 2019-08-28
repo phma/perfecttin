2aace111eb552e2b29ba0fb403efea4924f34281
Pierre Abbat <phma@bezitopo.org>
2019-08-28 08:08:38 +0000

    Translate new strings

--- perfecttin_en.ts
+++ perfecttin_en.ts
@@ -14,27 +14,32 @@
         <translation>Cancel</translation>
     </message>
     <message>
-        <location filename="configdialog.cpp" line="82"/>
+        <location filename="configdialog.cpp" line="86"/>
         <source>Input unit</source>
         <translation>Input unit</translation>
     </message>
     <message>
-        <location filename="configdialog.cpp" line="83"/>
+        <location filename="configdialog.cpp" line="87"/>
+        <source>Same units</source>
+        <translation>Same units</translation>
+    </message>
+    <message>
+        <location filename="configdialog.cpp" line="88"/>
         <source>Output unit</source>
         <translation>Output unit</translation>
     </message>
     <message>
-        <location filename="configdialog.cpp" line="84"/>
+        <location filename="configdialog.cpp" line="89"/>
         <source>Tolerance</source>
         <translation>Tolerance</translation>
     </message>
     <message>
-        <location filename="configdialog.cpp" line="85"/>
+        <location filename="configdialog.cpp" line="90"/>
         <source>Threads:</source>
         <translation>Threads:</translation>
     </message>
     <message numerus="yes">
-        <location filename="configdialog.cpp" line="86"/>
+        <location filename="configdialog.cpp" line="91"/>
         <source>default is %n</source>
         <translation>
             <numerusform>default is %n</numerusform>
@@ -42,7 +47,7 @@
         </translation>
     </message>
     <message>
-        <location filename="configdialog.cpp" line="87"/>
+        <location filename="configdialog.cpp" line="92"/>
         <source>Text mode DXF</source>
         <translation>Text mode DXF</translation>
     </message>
@@ -50,12 +55,12 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.cpp" line="83"/>
+        <location filename="mainwindow.cpp" line="84"/>
         <source>Making octagon</source>
         <translation>Making octagon</translation>
     </message>
     <message numerus="yes">
-        <location filename="mainwindow.cpp" line="85"/>
+        <location filename="mainwindow.cpp" line="86"/>
         <source>%n triangles</source>
         <translation>
             <numerusform>%n triangle</numerusform>
@@ -63,7 +68,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location filename="mainwindow.cpp" line="87"/>
+        <location filename="mainwindow.cpp" line="88"/>
         <source>%n dots</source>
         <translation>
             <numerusform>%n dot</numerusform>
@@ -71,74 +76,105 @@
         </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="167"/>
+        <location filename="mainwindow.cpp" line="168"/>
         <source>Load Point Cloud File</source>
         <translation>Load point cloud file</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="172"/>
+        <location filename="mainwindow.cpp" line="173"/>
         <source>(*.las);;(*.ply);;(*)</source>
         <translation>Laser files (*.las);;Polygon files (*.ply);;All files(*)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="174"/>
+        <location filename="mainwindow.cpp" line="175"/>
         <source>(*.las);;(*)</source>
         <translation>Laser files (*.las);;All files(*)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="202"/>
+        <location filename="mainwindow.cpp" line="203"/>
         <source>Convert to TIN</source>
         <translation>Convert to TIN</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="206"/>
+        <location filename="mainwindow.cpp" line="207"/>
         <source>*</source>
         <translation>All files (*)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="236"/>
+        <location filename="mainwindow.cpp" line="237"/>
+        <location filename="mainwindow.cpp" line="242"/>
+        <location filename="mainwindow.cpp" line="248"/>
+        <location filename="mainwindow.cpp" line="255"/>
         <source>PerfectTIN</source>
         <translation>PerfectTIN</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="236"/>
+        <location filename="mainwindow.cpp" line="237"/>
         <source>Point cloud no area</source>
         <translation>Point cloud covers no area or has infinite or NaN points</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="247"/>
+        <location filename="mainwindow.cpp" line="244"/>
+        <source>
+PLY file code © Simon Rajotte, MIT license</source>
+        <translation>
+PLY file code © Simon Rajotte, MIT license</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="249"/>
+        <source>%1
+Version %2
+Copyright %3 Pierre Abbat
+License GPL 3 or later%4</source>
+        <translation>%1
+Version %2
+Copyright %3 Pierre Abbat
+License GPL 3 or later%4</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="266"/>
         <source>&amp;File</source>
         <translation>&amp;File</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="248"/>
+        <location filename="mainwindow.cpp" line="267"/>
         <source>&amp;Settings</source>
         <translation>&amp;Settings</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="249"/>
+        <location filename="mainwindow.cpp" line="268"/>
         <source>&amp;Help</source>
         <translation>&amp;Help</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="253"/>
+        <location filename="mainwindow.cpp" line="272"/>
         <source>Load</source>
         <translation>Load</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="258"/>
+        <location filename="mainwindow.cpp" line="277"/>
         <source>Convert</source>
         <translation>Convert</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="263"/>
+        <location filename="mainwindow.cpp" line="282"/>
         <source>Clear</source>
         <translation>Clear</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="269"/>
+        <location filename="mainwindow.cpp" line="288"/>
         <source>Configure</source>
         <translation>Configure</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="293"/>
+        <source>About PerfectTIN</source>
+        <translation>About PerfectTIN</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="297"/>
+        <source>About Qt</source>
+        <translation>About Qt</translation>
     </message>
 </context>
 <context>
--- perfecttin_es.ts
+++ perfecttin_es.ts
@@ -14,27 +14,32 @@
         <translation>Anular</translation>
     </message>
     <message>
-        <location filename="configdialog.cpp" line="82"/>
+        <location filename="configdialog.cpp" line="86"/>
         <source>Input unit</source>
         <translation>Unidad de entrada</translation>
     </message>
     <message>
-        <location filename="configdialog.cpp" line="83"/>
+        <location filename="configdialog.cpp" line="87"/>
+        <source>Same units</source>
+        <translation>Mismas unidades</translation>
+    </message>
+    <message>
+        <location filename="configdialog.cpp" line="88"/>
         <source>Output unit</source>
         <translation>Unidad de salida</translation>
     </message>
     <message>
-        <location filename="configdialog.cpp" line="84"/>
+        <location filename="configdialog.cpp" line="89"/>
         <source>Tolerance</source>
         <translation>Tolerancia</translation>
     </message>
     <message>
-        <location filename="configdialog.cpp" line="85"/>
+        <location filename="configdialog.cpp" line="90"/>
         <source>Threads:</source>
         <translation>Hilos:</translation>
     </message>
     <message numerus="yes">
-        <location filename="configdialog.cpp" line="86"/>
+        <location filename="configdialog.cpp" line="91"/>
         <source>default is %n</source>
         <translation>
             <numerusform>%n predeterminado</numerusform>
@@ -42,7 +47,7 @@
         </translation>
     </message>
     <message>
-        <location filename="configdialog.cpp" line="87"/>
+        <location filename="configdialog.cpp" line="92"/>
         <source>Text mode DXF</source>
         <translation>DXF formato texto</translation>
     </message>
@@ -50,12 +55,12 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.cpp" line="83"/>
+        <location filename="mainwindow.cpp" line="84"/>
         <source>Making octagon</source>
         <translation>Haciendo octágono</translation>
     </message>
     <message numerus="yes">
-        <location filename="mainwindow.cpp" line="85"/>
+        <location filename="mainwindow.cpp" line="86"/>
         <source>%n triangles</source>
         <translation>
             <numerusform>%n triángulo</numerusform>
@@ -63,7 +68,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location filename="mainwindow.cpp" line="87"/>
+        <location filename="mainwindow.cpp" line="88"/>
         <source>%n dots</source>
         <translation>
             <numerusform>%n punto</numerusform>
@@ -71,74 +76,105 @@
         </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="167"/>
+        <location filename="mainwindow.cpp" line="168"/>
         <source>Load Point Cloud File</source>
         <translation>Cargar fichero de nube de puntos</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="172"/>
+        <location filename="mainwindow.cpp" line="173"/>
         <source>(*.las);;(*.ply);;(*)</source>
         <translation>Ficheros láser (*.las);;Ficheros polígono (*.ply);;Todos los ficheros(*)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="174"/>
+        <location filename="mainwindow.cpp" line="175"/>
         <source>(*.las);;(*)</source>
         <translation>Ficheros láser (*.las);;Todos los ficheros(*)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="202"/>
+        <location filename="mainwindow.cpp" line="203"/>
         <source>Convert to TIN</source>
         <translation>Convertir a TIN</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="206"/>
+        <location filename="mainwindow.cpp" line="207"/>
         <source>*</source>
         <translation>Todos los ficheros (*)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="236"/>
+        <location filename="mainwindow.cpp" line="237"/>
+        <location filename="mainwindow.cpp" line="242"/>
+        <location filename="mainwindow.cpp" line="248"/>
+        <location filename="mainwindow.cpp" line="255"/>
         <source>PerfectTIN</source>
         <translation>PerfectTIN</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="236"/>
+        <location filename="mainwindow.cpp" line="237"/>
         <source>Point cloud no area</source>
         <translation>Nube de puntos no cubre ninguna área o tiene puntos infinitos o NaN</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="247"/>
+        <location filename="mainwindow.cpp" line="244"/>
+        <source>
+PLY file code © Simon Rajotte, MIT license</source>
+        <translation>
+Código para ficheros PLY © Simon Rajotte, licencia MIT</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="249"/>
+        <source>%1
+Version %2
+Copyright %3 Pierre Abbat
+License GPL 3 or later%4</source>
+        <translation>%1
+Versión %2
+Copyright %3 Pierre Abbat
+Licencia GPL 3 o siguiente%4</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="266"/>
         <source>&amp;File</source>
         <translation>&amp;Fichero</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="248"/>
+        <location filename="mainwindow.cpp" line="267"/>
         <source>&amp;Settings</source>
         <translation>&amp;Ajustes</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="249"/>
+        <location filename="mainwindow.cpp" line="268"/>
         <source>&amp;Help</source>
         <translation>&amp;Ayuda</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="253"/>
+        <location filename="mainwindow.cpp" line="272"/>
         <source>Load</source>
         <translation>Cargar</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="258"/>
+        <location filename="mainwindow.cpp" line="277"/>
         <source>Convert</source>
         <translation>Convertir</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="263"/>
+        <location filename="mainwindow.cpp" line="282"/>
         <source>Clear</source>
         <translation>Vaciar</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="269"/>
+        <location filename="mainwindow.cpp" line="288"/>
         <source>Configure</source>
         <translation>Configurar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="293"/>
+        <source>About PerfectTIN</source>
+        <translation>Acerca de PerfectTIN</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="297"/>
+        <source>About Qt</source>
+        <translation>Acerca de Qt</translation>
     </message>
 </context>
 <context>
