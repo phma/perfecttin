ae287c5ae544ad8d9ce1241326c301e28f17265e
Pierre Abbat <phma@bezitopo.org>
2019-10-11 15:34:39 +0000

    Create new binary fuzzptin

---
+++ fuzz.cpp
@@ -0,0 +1,46 @@
+/******************************************************/
+/*                                                    */
+/* fuzz.cpp - fuzzing main program                    */
+/*                                                    */
+/******************************************************/
+/* Copyright 2019 Pierre Abbat.
+ * This file is part of PerfectTIN.
+ * 
+ * PerfectTIN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PerfectTIN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PerfectTIN. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "threads.h"
+#include "point.h"
+#include "octagon.h"
+#include "config.h"
+#include "fileio.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  int exitStatus;
+  int i,sz;
+  int nthreads;
+  string command,file;
+  if (argc>=2)
+  {
+    command=argv[1];
+    file=argv[2];
+  }
+  if (command=="readPtin")
+    readPtin(file);
+  exitStatus=0;
+  sz=net.convexHull.size();
+  return exitStatus;
+}
