9f62faae2a5d05936141abdb34a2c638567acebc
Pierre Abbat <phma@bezitopo.org>
2019-11-24 05:25:04 +0000

    Change license to LGPL

--- unitbutton.h
+++ unitbutton.h
@@ -5,19 +5,21 @@
 /******************************************************/
 /* Copyright 2019 Pierre Abbat.
  * This file is part of PerfectTIN.
- * 
+ *
  * PerfectTIN is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
  * PerfectTIN is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * GNU General Public License and Lesser General Public License
+ * for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with PerfectTIN. If not, see <http://www.gnu.org/licenses/>.
+ * and Lesser General Public License along with PerfectTIN. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 #ifndef UNITBUTTON_H
 #define UNITBUTTON_H
