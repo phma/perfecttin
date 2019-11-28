2e7bf03dec90228ecee5f6566d284beeb029c794
Pierre Abbat <phma@bezitopo.org>
2019-11-28 09:19:07 +0000

    Remove "modify" from "modify modify"

--- landxml.h
+++ landxml.h
@@ -7,7 +7,7 @@
  * This file is part of PerfectTIN.
  *
  * PerfectTIN is free software: you can redistribute it and/or modify
- * modify it under the terms of the GNU Lesser General Public License as
+ * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
