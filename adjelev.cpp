4514d4fbff93cec9db6377f8d3d5734e2b5349a1
Pierre Abbat <phma@bezitopo.org>
2020-02-20 20:19:55 +0000

    If there are no dots in the triangles being adjusted, call it singular
    adjustElev uses minimum norm in this case, resulting in the adjustments being
    zero. Least squares would have set the adjustments to infinity or NaN.
    This seems to fix the non-Delaunay edges.

--- adjelev.cpp
+++ adjelev.cpp
@@ -135,7 +135,7 @@ adjustRecord adjustElev(vector<triangle *> tri,vector<point *> pnt)
     localClipLow=pointClipLow;
   for (k=0;k<pnt.size();k++)
   {
-    if (std::isfinite(x[k]) && fabs(x[k])<16384)
+    if (std::isfinite(x[k]) && fabs(x[k])<16384 && ndots)
     /* Ground elevation is in the range [-11000,8850]. Roundoff error in a
      * singular matrix produces numbers around 1e18. Any number too big
      * means that the matrix is singular, even if the number is finite.
