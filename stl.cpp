0b77b623494fab3e7f557849a17d7dfb33590d88
Pierre Abbat <phma@bezitopo.org>
2020-08-09 19:44:27 +0000

    Add STL triangle class

---
+++ stl.cpp
@@ -0,0 +1,46 @@
+/******************************************************/
+/*                                                    */
+/* stl.cpp - stereolithography (3D printing) export   */
+/*                                                    */
+/******************************************************/
+/* Copyright 2020 Pierre Abbat.
+ * This file is part of PerfectTIN.
+ *
+ * PerfectTIN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * PerfectTIN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and Lesser General Public License along with PerfectTIN. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include "stl.h"
+using namespace std;
+
+/* The STL polyhedron consists of three kinds of face: bottom, side, and top.
+ * The bottom is a convex polygon which is triangulated. The sides are
+ * trapezoids, each of which is drawn as two triangles. The top is the
+ * TIN surface.
+ */
+
+stltriangle::stltriangle()
+{
+  normal=a=b=c=xyz(0,0,0);
+}
+
+stltriangle::stltriangle(xyz A,xyz B,xyz C)
+{
+  a=A;
+  b=B;
+  c=C;
+  normal=cross(a-b,b-c);
+  normal.normalize();
+}
---
+++ stl.h
@@ -0,0 +1,36 @@
+/******************************************************/
+/*                                                    */
+/* stl.h - stereolithography (3D printing) export     */
+/*                                                    */
+/******************************************************/
+/* Copyright 2020 Pierre Abbat.
+ * This file is part of PerfectTIN.
+ *
+ * PerfectTIN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * PerfectTIN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and Lesser General Public License along with PerfectTIN. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include <array>
+#include <vector>
+#include "point.h"
+#include "config.h"
+
+struct stltriangle
+{
+  xyz normal,a,b,c;
+  std::string attributes;
+  stltriangle();
+  stltriangle(xyz A,xyz B,xyz C);
+};
