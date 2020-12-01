b4b4b98c621621d4ad9d6e7f3b1aa4f32c14f986
Pierre Abbat <phma@bezitopo.org>
2020-12-01 09:00:19 +0000

    Update copyright on boundrect

--- boundrect.cpp
+++ boundrect.cpp
@@ -3,7 +3,7 @@
 /* boundrect.cpp - bounding rectangles                */
 /*                                                    */
 /******************************************************/
-/* Copyright 2019 Pierre Abbat.
+/* Copyright 2019,2020 Pierre Abbat.
  * This file is part of PerfectTIN.
  *
  * PerfectTIN is free software: you can redistribute it and/or modify
