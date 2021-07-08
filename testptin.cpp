950333e8ae73fa129151a05c2fdf85fc8804a5cd
Pierre Abbat <phma@bezitopo.org>
2021-07-08 08:27:43 +0000

    Prune contours in the contour test

--- testptin.cpp
+++ testptin.cpp
@@ -1491,6 +1491,8 @@ void testcontour()
   net.setCurrentContours(ci);
   roughcontours(net,ci.mediumInterval());
   drawNet(ps);
+  prunecontours(net,ci.tolerance());
+  drawNet(ps);
   for (areaAfter=i=0;i<net.triangles.size();i++)
   {
     tassert(net.triangles[i].sarea>1);
@@ -1499,8 +1501,6 @@ void testcontour()
   dots3after=net.triangles[3].dots.size();
   dots6=net.triangles[6].dots.size();
   dots7=net.triangles[7].dots.size();
-  cout<<"After: "<<dots3after<<" dots in 3 "<<dots6<<" dots in 6 "<<dots7<<" dots in 7\n";
-  cout<<"Area "<<areaBefore<<" before, "<<areaAfter<<" after\n";
   ps.close();
 }
 
