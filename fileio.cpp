f1bbb000d4efec0c39f32bcd95070dab01254561
Pierre Abbat <phma@bezitopo.org>
2021-07-28 17:49:04 +0000

    Remove debugging statements and set checksum tolerance back to 5

--- fileio.cpp
+++ fileio.cpp
@@ -907,8 +907,7 @@ PtinHeader readPtin(std::string inputFile)
 	      header.tolRatio=PT_CONTOUR_ERROR;
 	    }
 	    concheck=readleint(ptinFile);
-	    cout<<foldangle(concheck-ctour.checksum())<<" concheck\n";
-	    if (abs(foldangle(concheck-ctour.checksum()))>555)
+	    if (abs(foldangle(concheck-ctour.checksum()))>5)
 	      header.tolRatio=PT_CONTOUR_ERROR;
 	    net.contours[ci].push_back(ctour);
 	  }
@@ -931,7 +930,6 @@ PtinHeader readPtin(std::string inputFile)
 	      header.tolRatio=PT_CONTOUR_ERROR;
 	    }
 	    concheck=readleint(ptinFile);
-	    cout<<foldangle(concheck-net.boundary.checksum())<<" concheck\n";
 	    if (abs(foldangle(concheck-net.boundary.checksum()))>5)
 	      header.tolRatio=PT_CONTOUR_ERROR;
 	  }
